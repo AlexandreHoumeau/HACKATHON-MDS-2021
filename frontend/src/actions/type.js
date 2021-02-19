592c96deb65b0d4d09855c5a6741433e1f3c313b
Alexandre Houmeau <alexandre.houmeau@gmail.com>
2021-02-19 11:06:16 +0100

    [!]: removed frontend

--- frontend/src/actions/type.js
+++
@@ -1,5 +0,0 @@
-export const GET_ERRORS = "GET_ERRORS";
-export const USER_LOADING = "USER_LOADING";
-export const SET_CURRENT_USER = "SET_CURRENT_USER";
-export const CONTACT_USER ="CONTACT_USER"
-export const SEARCH_RESULT = "SEARCH_RESULT"
